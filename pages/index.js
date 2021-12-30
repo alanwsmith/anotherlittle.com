9299064ede2ef231405f408c45d638b27bc1ef4f
Alan W. Smith <alan@alanwsmith.com>
2021-12-30 12:28:13 -0500

    Add podcast link

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,18 @@ export default function Home() {
         </div>
         <div className="tiny text-right pr-2 text-gray-400">
           That&apos;s it. That&apos;s the site. From{' '}
-          <a href="https://www.alanwsmith.com/" className="text-blue-400">
+          <a href="https://twitter.com/TheIdOfAlan" className="text-blue-500">
             Alan W. Smith
           </a>{' '}
-          for{' '}
-          <a href="https://dusty.domains/" className="text-blue-400">
+          (who also does{' '}
+          <a
+            href="https://www.alanwsmith.com/the-pod-of-alan"
+            className="text-blue-500"
+          >
+            a podcast
+          </a>
+          ) for{' '}
+          <a href="https://dusty.domains/" className="text-blue-500">
             Dusty Domains 2021
           </a>
         </div>
